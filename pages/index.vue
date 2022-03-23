a1eff564942ed4a9ef2015531bf549cd49ec25d5
Leng Chhinghor <lengchhinghor@gmail.com>
2022-03-23 09:51:51 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 <template>
     <div id="IndexPage">
         <button class='btn' @click='getUserAgeFeatureStatus'>
-            Calculate Your Age
+            Calculate Age
         </button>
 
         <p v-if='!userAgeFeature && !loading' class='text'>Sorry, This feature has been disabled by the Admin</p>
